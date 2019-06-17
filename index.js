ec06d7139c58f8e3222b89f50c23d2962ba6ce35
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-17 19:59:15 +0000

    Deploy AlexPFrank/TelegramBot_EXAM to github.com/AlexPFrank/TelegramBot_EXAM.git:gh-pages

---
+++ index.js
@@ -0,0 +1,49 @@
+const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
+const config = require('./config.json');
+
+const token = $BOT_TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.onText(/\/start/,(msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Выберите валюту, которая вас интересует:', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: '€ - EUR',
+            callback_data: 'EUR'
+          },
+          {
+            text: '$ - USD',
+            callback_data: 'USD'
+          },
+          {
+            text: '₽ - RUR',
+            callback_data: 'RUR'
+          },
+          {
+            text: '₿ - BTC',
+            callback_data: 'BTC'
+          }
+        ]
+      ]
+    }
+  });
+});
+
+bot.on('callback_query', query => {
+  const id = query.message.chat.id;
+  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function(error, response, body){
+    const data = JSON.parse(body);
+    const result = data.filter(item => item.ccy === query.data)[0];
+    let md = `
+    *${result.ccy} => ${result.base_ccy}*
+    Купля: _${result.buy}_
+    Продажа: _${result.sale}_ `;
+    bot.sendMessage(id, md, {parse_mode: 'Markdown'});
+  });
+});
